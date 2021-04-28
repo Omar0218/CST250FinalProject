cf4a30ade606f52b3def2cf03b2cb592305ca877
Omar0218 <72273360+Omar0218@users.noreply.github.com>
2021-04-27 22:52:05 -0500

    Add files via upload

---
+++ FinalProject.MainClass.java
@@ -0,0 +1,12 @@
+
+package finalproject;
+
+public class MainClass {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Hello! this is the start of my final project!");
+        
+    }
+    
+}
